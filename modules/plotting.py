b791e351a8588b27052c7a589fafaa2876a9da8a
Arindam <aqd5773@psu.edu>
2023-03-03 15:26:46 -0500

    added radar plot

--- modules/plotting.py
+++ modules/plotting.py
@@ -19,3 +19,33 @@ def plot(points,dset, title,  thresh=0.01):
     ax[3].hist(ne, np.arange(0, 12), width=0.80)
     plt.show()
     plt.close()
+
+class Radar(object):
+    def __init__(self, figure, title, labels, rect=None):
+        if rect is None:
+            rect = [0.05, 0.05, 0.9, 0.9]
+
+        self.n = len(title)
+        self.angles = np.arange(0, 360, 360.0/self.n)
+
+        self.axes = [figure.add_axes(rect, projection='polar', label='axes%d' % i) for i in range(self.n)]
+
+        self.ax = self.axes[0]
+        self.ax.set_thetagrids(self.angles, labels=title, fontsize=12)
+
+        for ax in self.axes[1:]:
+            ax.patch.set_visible(False)
+            ax.grid(False)
+            ax.xaxis.set_visible(False)
+
+        for ax, angle, label in zip(self.axes, self.angles, labels):
+            ax.set_rgrids(range(0, 6), angle=angle, labels=label,fontsize = 8)
+            ax.spines['polar'].set_visible(False)
+            ax.set_ylim(0, 5)
+            
+            
+    def plot(self, values, *args, **kw):
+        angle = np.deg2rad(np.r_[self.angles, self.angles[0]])
+        values = np.r_[values, values[0]]
+        self.ax.plot(angle, values, *args, **kw)
+    
